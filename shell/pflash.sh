7c95524ce380be267a54527bc374d609d5abacbc
llb <lilubao@gionee.com>
2016-12-01 15:37:50 +0800

    add some shell

---
+++ shell/pflash.sh
@@ -0,0 +1,33 @@
+
+fastboot flash modem NON-HLOS.bin
+fastboot flash splash splash.img
+fastboot flash sbl1 sbl1.mbn
+fastboot flash sbl1bak sbl1.mbn
+fastboot flash rpm  rpm.mbn
+fastboot flash rpmbak rpm.mbn
+fastboot flash tz tz.mbn
+fastboot flash tzbak tz.mbn
+fastboot flash devcfg devcfg.mbn
+fastboot flash devcfgbak devcfg.mbn
+
+fastboot flash dsp adspso.bin
+fastboot flash aboot emmc_appsboot.mbn
+fastboot flash abootbak emmc_appsboot.mbn
+fastboot flash boot  boot.img
+fastboot flash recovery recovery.img
+fastboot flash system system.img
+fastboot flash cache cache.img
+fastboot flash persist persist.img
+fastboot flash mdtp mdtp.img
+
+
+fastboot flash cmnlib cmnlib.mbn
+fastboot flash cmnlibbak cmnlib.mbn
+
+fastboot flash cmnlib64 cmnlib64.mbn
+fastboot flash cmnlib64bak cmnlib64.mbn
+fastboot flash keymaster keymaster.mbn
+fastboot flash keymasterbak  keymaster.mbn
+
+fastboot flash userdata userdata.img
+
