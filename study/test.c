ddf82ee5d21e95c0c6ded7039438f39b9bb5fbca
llb <lilubao@gionee.com>
2017-01-09 10:04:51 +0800

    for test #define

--- study/test.c
+++ study/test.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+/*
+ * #define的用法：
+ *定义一个跟行号相关的变量
+ */
+
+//为什么不能直接定义为DD(A)  BB(A,__LINE__)
+
+#define BB(B,C) B##C
+#define AA(B,C) BB(B,C)
+#define DD(A)   AA(A,__LINE__)
+
+
 int main()
 {
-    printf("1111111111\n");
-    
-    for(;;)
-            ;
-        
-    printf("2222222222\n");
+    printf("line--->%d\n",DD(1));
 
 
     return 0;
