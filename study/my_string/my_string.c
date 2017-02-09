97a410547580f993b298e3a3f00156c23536531c
llb <lilubao@gionee.com>
2017-02-09 09:11:49 +0800

    2017.2.9 add 17G05A.c for start 17G05A project

--- study/my_string/my_string.c
+++ study/my_string/my_string.c
@@ -4,15 +4,26 @@
 #include <stdio.h>
 
 /*字符串转化为整形*/
-int my_atoi(char str)
+int my_atoi(char * str)
 {
+    int num=0;
+    
+    do{
+        
+        num= num+10+(*str-'0');
 
-    return 1;
+    }while( (str++)!=NULL  );
+
+    return num;
 }
 
 int main()
 {
     printf("hello world\n");
+    
+    char str_num[]="123456";
+    
+    printf("str_num=%d\n",my_atoi(str_num));
 
     return 0;
 }
