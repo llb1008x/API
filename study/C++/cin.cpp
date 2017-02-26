d7e7c220296ca0ec065f84ebec72dba1a77b7244
llb_hp <llb1008x@126.com>
2017-02-26 08:57:15 +0800

    add 2017.2.26 daily plan

--- study/C++/cin.cpp
+++ study/C++/cin.cpp
@@ -1,21 +1,29 @@
+/*********************************************************
+		cin ,getline的区别
+cin是以空格，tab，换行为结束的
+getline是以换行为结束
+*********************************************************/
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
+
 int main()
 {
-    char str1[10],str2[10];
 
-    memset(str1,10,0);
-    memset(str2,10,0);
+	char name1[20]="",name2[20]="";
 
-    cin.getline(str1,10);
-    cin.getline(str2,10);
-   
-    cout<<"str1"<<str1<<endl;
-    cout<<"str2"<<str2<<endl;
+#if    0
+	cin>>name1>>name2;
+
+#else
+	cin.getline(name1,20);
+	cin.getline(name2,20);
+		
+
+#endif
+	cout<<"name1:"<<name1<<"name2:"<<name2<<endl;
 
 
-    return 0;
+	return 0;
 }
