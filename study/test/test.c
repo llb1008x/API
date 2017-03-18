2c3a19e343366a304e04992a929ef454ee5de5a8
llb_hp <llb1008x@126.com>
2017-03-18 23:41:18 +0800

    study for algo link

--- study/test/test.c
+++ study/test/test.c
@@ -4,31 +4,29 @@
 	> Mail: llb1008x@126.com 
 	> Created Time: 2017年03月10日 星期五 16时58分30秒
 
-	输入一个数，输出他的逆序数
+ getint()
+ 输入一个字符流，输出对应的整数
  ************************************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int getch(void );
+void ungetch(int );
+
+int getint(int *pn)
+{
+	int c,sign;
+
+	while()
+
+
+}
 
 
 int main()
 {
-	int a,b;
-	int num[3];
 
-	scanf("%d",&a);
-
-	if( a>999 ){
-
-		printf("error\n");
-		return -1;
-	}
-
-	num[0]=a/100;
-	num[1]=a%100/10;
-	num[2]=a%10;
-	/*数值位数逆序*/
-	b=num[0]+num[1]*10+num[2]*100;
-	printf("b->%d\n",b);
 
 	return 0;
 }
