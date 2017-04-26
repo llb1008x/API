c91c58fb81866932b3433dc2ef16c9674b469722
llb_hp <llb1008x@126.com>
2017-04-26 23:47:34 +0800

    add today study

---
+++ study/C/class_clang/day_6/test.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: llb
+	> Mail: llb1008x@126.com 
+	> Created Time: 2017年04月26日 星期三 22时45分01秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include <limits.h>
+
+int main()
+{
+
+	printf("hello world\n");
+	printf("maximum->%d\n",INT_MAX);
+
+
+	return 0;
+}
