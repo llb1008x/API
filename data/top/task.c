6c2844103c1fecdbcbc58c6d5065b1b2c2f12b5c
llb <lilubao@gionee.com>
2017-05-09 17:39:09 +0800

    add today work

--- data/top/task.c
+++ data/top/task.c
@@ -290,153 +290,163 @@
 
 
 
-//==============================================================================
-// PMIC define
-//==============================================================================
-typedef enum {
-	AUXADC_LIST_BATADC,     //检测电池
-	AUXADC_LIST_VCDT,       //VCDT：充电电压检测脚
-	AUXADC_LIST_BATTEMP,    //电池温度
-	AUXADC_LIST_BATID,      //battery ID
-	AUXADC_LIST_VBIF,       //应该是跟USB相关的
-	AUXADC_LIST_CHIP_TEMP,  //芯片温度
-	AUXADC_LIST_DCXO,       // 数字温度补偿晶体震荡器
-	AUXADC_LIST_ACCDET,     //耳机检测
-	AUXADC_LISTTSX,        
-	AUXADC_LIST_HP,
-} AUXADC_LIST;
-auxadc定义的采样channel
+    //==============================================================================
+    // PMIC define
+    //==============================================================================
+    typedef enum {
+        AUXADC_LIST_BATADC,     //检测电池
+        AUXADC_LIST_VCDT,       //VCDT：充电电压检测脚
+        AUXADC_LIST_BATTEMP,    //电池温度
+        AUXADC_LIST_BATID,      //battery ID
+        AUXADC_LIST_VBIF,       //应该是跟USB相关的
+        AUXADC_LIST_CHIP_TEMP,  //芯片温度
+        AUXADC_LIST_DCXO,       // 数字温度补偿晶体震荡器
+        AUXADC_LIST_ACCDET,     //耳机检测
+        AUXADC_LISTTSX,        
+        AUXADC_LIST_HP,
+    } AUXADC_LIST;
+    auxadc定义的采样channel
 
 
-G1605A上电池相关参数在battery_common.h mt_battery_meter.h上，但是17G05A这些参数在哪？
-mtk_charging.h，mtk_battery_meter.h
+    G1605A上电池相关参数在battery_common.h mt_battery_meter.h上，但是17G05A这些参数在哪？
+    mtk_charging.h，mtk_battery_meter.h
 
 
-/*****************************************************************************************************
-现在的问题是恒流充电的电流太小，0%关机电压太高，开机充电过程中会出现关机的情况，电量计系数要等电池曲线,关机充电是否有问题
+    /*****************************************************************************************************
+    现在的问题是恒流充电的电流太小，0%关机电压太高，开机充电过程中会出现关机的情况，电量计系数要等电池曲线,关机充电是否有问题
+    弄清楚battery_probe这个里面几个中断回调函数和创建的线程想干什么？
+    马达刷机第一次震动太弱，之后震动正常
+    ******************************************************************************************************/
+    0%关机电压太高
+    {
+        这个应该需要修改电量0%参数
+        mtk_battery_meter.h ，mtk_battery_property.h
 
-弄清楚battery_probe这个里面几个中断回调函数和创建的线程想干什么？
-******************************************************************************************************/
-0%关机电压太高
-{
-    这个应该需要修改电量0%参数
-    mtk_battery_meter.h ，mtk_battery_property.h
+        mtk_battery_property.h这个里面包含很多电池电量计相关的参数
 
-    mtk_battery_property.h这个里面包含很多电池电量计相关的参数
-    //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process begin
-    #define SHUTDOWN_GAUGE0 	  0 	// 1->0			
-    #define SHUTDOWN_GAUGE1_XMINS 1
-    #define SHUTDOWN_GAUGE0_VOLTAGE 	34000	// 35000->34000
-    //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process end
-
-修改：
-    1.关机的判断条件有很多，这里通常电量为0%，或者低于关机电压
-    一般上报电量0%的时候，上层会下达关机的指令，然后层层调用进行关机，有时候也会是检测到关机条件后底层直接关机的
-    SHUTDOWN_GAUGE0这个是不支持0%关机，只是电压低于关机电压后关机这个条件
-    SHUTDOWN_GAUGE0_VOLTAGE这个定义的关机电压
-
-    2. fg_cust_data.pseudo1_t0 = UNIT_TRANS_100 * g_FG_PSEUDO1_T0[g_fg_battery_id];
-    这个根据battery_id选择底层多少的偏移量相当于上层的1% ，100%
-    //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process begin
-    int g_FG_PSEUDO1_T0[TOTAL_BATTERY_NUMBER] = { 2, 6, 7, 8};		// 5->2
-    int g_FG_PSEUDO1_T1[TOTAL_BATTERY_NUMBER] = { 3, 10, 11, 12};	// 9->3
-    int g_FG_PSEUDO1_T2[TOTAL_BATTERY_NUMBER] = { 13, 14, 15, 16};
-    int g_FG_PSEUDO1_T3[TOTAL_BATTERY_NUMBER] = { 17, 18, 19, 20};
-    int g_FG_PSEUDO1_T4[TOTAL_BATTERY_NUMBER] = { 21, 22, 23, 24};
-
-    int g_FG_PSEUDO100_T0[TOTAL_BATTERY_NUMBER] = { 98, 98, 98, 97};
-    int g_FG_PSEUDO100_T1[TOTAL_BATTERY_NUMBER] = { 98, 95, 94, 93};// 95->98	
-    int g_FG_PSEUDO100_T2[TOTAL_BATTERY_NUMBER] = { 90, 90, 90, 89};
-    int g_FG_PSEUDO100_T3[TOTAL_BATTERY_NUMBER] = { 80, 80, 86, 85};
-    int g_FG_PSEUDO100_T4[TOTAL_BATTERY_NUMBER] = { 80, 80, 82, 81};
-    //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process end
-}
+        shutdown_event_handler做了哪些事：
+        {   
+        1.执行关机的回调函数，判断一些关机条件是否满足或触发然后执行kernel_power_off -> machine_power_off
+        mtk_power_misc_init     power_misc      power_misc_thread
 
 
+        2.会创建一个power_misc_thread线程，这个进程应该是在低电条件下创建的，低电出发了条件，高电的时候没有这个
+        这个是关机的条件 
+            struct shutdown_condition {
+                bool is_overheat;
+                bool is_soc_zero_percent;
+                bool is_uisoc_one_percent;
+                bool is_under_shutdown_voltage;
+                bool is_dlpt_shutdown;
+            };
 
-调用流程    
-电池相关参数的初始化，这部分跟电池参数相关的应该是之前的meter那一类
-（mtk_battery.c）battery_probe 电池参数的初始化 -> (mtk_battery_hal.c)battery_meter_ctrl = bm_ctrl_cmd,电池电量参数相关计算的一组函数接口
+            /* ============================================================ */
+            /* power misc related */
+            /* ============================================================ */
+            #define BAT_VOLTAGE_LOW_BOUND 3400
+            #define BAT_VOLTAGE_HIGH_BOUND 3450
+            #define SHUTDOWN_TIME 40
+            #define AVGVBAT_ARRAY_SIZE 30
+            #define INIT_VOLTAGE 3450
+            #define BATTERY_SHUTDOWN_TEMPERATURE 60
 
--> fg_custom_init_from_header 从头文件中获得电池参数 -> battery_update_routine,这是一个线程，是负责检测硬件电池参数变化，然后上报给power_supply
+        3.shutdown_event_handler  
+            这里面都是对那些关机条件进行判断，符合条件的轮训一段时间然后判断是否大于SHUTDOWN_TIME
+            符合条件就  kernel_power_off
 
-根据debug等级可以调整轮训时间 bat_get_debug_level() >= BMLOG_DEBUG_LEVEL -> (mtk_power_misc.c)shutdown_event_handler这个是关机检测执行的回调函数
+        4.main.log里面搜索ShutdownThread，上层传达的关机流程
+        01-01 07:43:19.967753  2009  2460 V MediaScannerService: handleShutdownThreadpool()
+        01-01 07:57:58.074049   962  1080 D ShutdownThread: !!! Request to shutdown !!!
+        01-01 07:57:58.074354   962  1080 D ShutdownThread:     |----com.android.server.power.ShutdownThread.shutdown(ShutdownThread.java:201)
+        01-01 07:57:58.074429   962  1080 D ShutdownThread:     |----com.android.server.power.PowerManagerService$9.run(PowerManagerService.java:2731)
+        01-01 07:57:58.074473   962  1080 D ShutdownThread:     |----android.os.Handler.handleCallback(Handler.java:836)
+        01-01 07:57:58.074521   962  1080 D ShutdownThread:     |----android.os.Handler.dispatchMessage(Handler.java:103)
+        01-01 07:57:58.074551   962  1080 D ShutdownThread:     |----android.os.Looper.loop(Looper.java:203)
+        01-01 07:57:58.074596   962  1080 D ShutdownThread:     |----android.os.HandlerThread.run(HandlerThread.java:61)
+        01-01 07:57:58.074639   962  1080 D ShutdownThread:     |----com.android.server.ServiceThread.run(ServiceThread.java:46)
+        01-01 07:57:58.074789   962  1080 D ShutdownThread: Notifying thread to start shutdown longPressBehavior=1
+        01-01 07:57:58.079296   962  1080 D ShutdownThread: IBootAnim get screenTurnOffTime : 5000
+        01-01 07:57:58.134895   962  3204 D ShutdownThread: ro.crypto.state: unencrypted
+        01-01 07:57:58.137951   962  3204 D ShutdownThread: checkShutdownFlow: IPO_Support=true mReboot=false sys.ipo.disable= isSafeMode=false passEncryptionCondition=true
+        01-01 07:57:58.138904   962  3204 D ShutdownThread: checkShutdownFlow: isIPOEnabled=true mShutdownFlow=0
+        01-01 07:57:58.145422   962  3204 I ShutdownThread: set launcher as foreground
+        01-01 07:57:58.172181   962  3204 I ShutdownThread: Sending shutdown broadcast...
+        01-01 07:57:58.501727   962  3204 I ShutdownThread: Shutting down activity manager...
+        01-01 07:57:58.710893   962  3204 I ShutdownThread: Shutting down package manager...
+        01-01 07:57:58.720345   962  3204 I ShutdownThread: Shutting down radios...
+        01-01 07:57:58.722303   962  3220 W ShutdownThread: task run
+        01-01 07:57:58.725504   962  3220 W ShutdownThread: Turning off cellular radios...
+        01-01 07:57:58.744406   962  3220 I ShutdownThread: Waiting for NFC, Bluetooth and Radio...
+        01-01 07:57:59.247395   962  3220 I ShutdownThread: Radio turned off.
+        01-01 07:57:59.248435   962  3220 I ShutdownThread: NFC, Radio and Bluetooth shutdown complete.
+        01-01 07:57:59.249857   962  3204 I ShutdownThread: Shutting down MountService
+        01-01 07:58:03.139331   962  1080 D ShutdownThread: setBacklightBrightness: Off
+        
+        }
 
 
+    修改：
+        1.关机的判断条件有很多，这里通常电量为0%，或者低于关机电压
+        一般上报电量0%的时候，上层会下达关机的指令，然后层层调用进行关机，有时候也会是检测到关机条件后底层直接关机的
+        SHUTDOWN_GAUGE0这个是不支持0%关机，只是电压低于关机电压后关机这个条件
+        SHUTDOWN_GAUGE0_VOLTAGE这个定义的关机电压
+
+        2. fg_cust_data.pseudo1_t0 = UNIT_TRANS_100 * g_FG_PSEUDO1_T0[g_fg_battery_id];
+        这个根据battery_id选择底层多少的偏移量相当于上层的1% ，100%
+        //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process begin
+        int g_FG_PSEUDO1_T0[TOTAL_BATTERY_NUMBER] = { 2, 6, 7, 8};		// 5->2
+        int g_FG_PSEUDO1_T1[TOTAL_BATTERY_NUMBER] = { 3, 10, 11, 12};	// 9->3
+        int g_FG_PSEUDO1_T2[TOTAL_BATTERY_NUMBER] = { 13, 14, 15, 16};
+        int g_FG_PSEUDO1_T3[TOTAL_BATTERY_NUMBER] = { 17, 18, 19, 20};
+        int g_FG_PSEUDO1_T4[TOTAL_BATTERY_NUMBER] = { 21, 22, 23, 24};
+
+        int g_FG_PSEUDO100_T0[TOTAL_BATTERY_NUMBER] = { 98, 98, 98, 97};
+        int g_FG_PSEUDO100_T1[TOTAL_BATTERY_NUMBER] = { 98, 95, 94, 93};// 95->98	
+        int g_FG_PSEUDO100_T2[TOTAL_BATTERY_NUMBER] = { 90, 90, 90, 89};
+        int g_FG_PSEUDO100_T3[TOTAL_BATTERY_NUMBER] = { 80, 80, 86, 85};
+        int g_FG_PSEUDO100_T4[TOTAL_BATTERY_NUMBER] = { 80, 80, 82, 81};
+        //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process end
+
+        //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process begin
+        #define SHUTDOWN_GAUGE0 	  0 	// 1->0			
+        #define SHUTDOWN_GAUGE1_XMINS 1
+        #define SHUTDOWN_GAUGE0_VOLTAGE 	34000	// 35000->34000
+        //Gionee <gn_by_charging> <lilubao> <20170508> add for change charging process end
+    }
 
 
-shutdown_event_handler做了哪些事：
-{   
-    1.执行关机的回调函数，判断一些关机条件是否满足或触发然后执行kernel_power_off -> machine_power_off
-    mtk_power_misc_init     power_misc      power_misc_thread
+    恒流充电电流太小只有 1.3~1.6A左右 
+    {
+        之前修改的部分没有同步，试试改过之后还是否这样
 
-
-    2.会创建一个power_misc_thread线程，这个进程应该是在低电条件下创建的，低电出发了条件，高电的时候没有这个
-       这个是关机的条件 
-        struct shutdown_condition {
-            bool is_overheat;
-            bool is_soc_zero_percent;
-            bool is_uisoc_one_percent;
-            bool is_under_shutdown_voltage;
-            bool is_dlpt_shutdown;
-        };
-
-        /* ============================================================ */
-        /* power misc related */
-        /* ============================================================ */
-        #define BAT_VOLTAGE_LOW_BOUND 3400
-        #define BAT_VOLTAGE_HIGH_BOUND 3450
-        #define SHUTDOWN_TIME 40
-        #define AVGVBAT_ARRAY_SIZE 30
-        #define INIT_VOLTAGE 3450
-        #define BATTERY_SHUTDOWN_TEMPERATURE 60
-
-    3.shutdown_event_handler  
-        这里面都是对那些关机条件进行判断，符合条件的轮训一段时间然后判断是否大于SHUTDOWN_TIME
-        符合条件就  kernel_power_off
-
-    4.main.log里面搜索ShutdownThread，上层传达的关机流程
-    01-01 07:43:19.967753  2009  2460 V MediaScannerService: handleShutdownThreadpool()
-    01-01 07:57:58.074049   962  1080 D ShutdownThread: !!! Request to shutdown !!!
-    01-01 07:57:58.074354   962  1080 D ShutdownThread:     |----com.android.server.power.ShutdownThread.shutdown(ShutdownThread.java:201)
-    01-01 07:57:58.074429   962  1080 D ShutdownThread:     |----com.android.server.power.PowerManagerService$9.run(PowerManagerService.java:2731)
-    01-01 07:57:58.074473   962  1080 D ShutdownThread:     |----android.os.Handler.handleCallback(Handler.java:836)
-    01-01 07:57:58.074521   962  1080 D ShutdownThread:     |----android.os.Handler.dispatchMessage(Handler.java:103)
-    01-01 07:57:58.074551   962  1080 D ShutdownThread:     |----android.os.Looper.loop(Looper.java:203)
-    01-01 07:57:58.074596   962  1080 D ShutdownThread:     |----android.os.HandlerThread.run(HandlerThread.java:61)
-    01-01 07:57:58.074639   962  1080 D ShutdownThread:     |----com.android.server.ServiceThread.run(ServiceThread.java:46)
-    01-01 07:57:58.074789   962  1080 D ShutdownThread: Notifying thread to start shutdown longPressBehavior=1
-    01-01 07:57:58.079296   962  1080 D ShutdownThread: IBootAnim get screenTurnOffTime : 5000
-    01-01 07:57:58.134895   962  3204 D ShutdownThread: ro.crypto.state: unencrypted
-    01-01 07:57:58.137951   962  3204 D ShutdownThread: checkShutdownFlow: IPO_Support=true mReboot=false sys.ipo.disable= isSafeMode=false passEncryptionCondition=true
-    01-01 07:57:58.138904   962  3204 D ShutdownThread: checkShutdownFlow: isIPOEnabled=true mShutdownFlow=0
-    01-01 07:57:58.145422   962  3204 I ShutdownThread: set launcher as foreground
-    01-01 07:57:58.172181   962  3204 I ShutdownThread: Sending shutdown broadcast...
-    01-01 07:57:58.501727   962  3204 I ShutdownThread: Shutting down activity manager...
-    01-01 07:57:58.710893   962  3204 I ShutdownThread: Shutting down package manager...
-    01-01 07:57:58.720345   962  3204 I ShutdownThread: Shutting down radios...
-    01-01 07:57:58.722303   962  3220 W ShutdownThread: task run
-    01-01 07:57:58.725504   962  3220 W ShutdownThread: Turning off cellular radios...
-    01-01 07:57:58.744406   962  3220 I ShutdownThread: Waiting for NFC, Bluetooth and Radio...
-    01-01 07:57:59.247395   962  3220 I ShutdownThread: Radio turned off.
-    01-01 07:57:59.248435   962  3220 I ShutdownThread: NFC, Radio and Bluetooth shutdown complete.
-    01-01 07:57:59.249857   962  3204 I ShutdownThread: Shutting down MountService
-    01-01 07:58:03.139331   962  1080 D ShutdownThread: setBacklightBrightness: Off
-    
-}
-
-fg这两个中断干什么？
-{
-    fg_iavg_int_ht_handler      FG_IAVG_H_NO
-    fg_iavg_int_lt_handler      FG_IAVG_L_NO
-
-    应该是跟high，low相关的
-}
+        修改之后电流可以达到1.9A但是不是一直这样，然后电流逐渐下降，这不是恒流充电该有的
+    }
 
 
 
-控制fuel_gauge的log等级
-FG_daemon_log_level
+    调用流程    
+    电池相关参数的初始化，这部分跟电池参数相关的应该是之前的meter那一类
+    （mtk_battery.c）battery_probe 电池参数的初始化 -> (mtk_battery_hal.c)battery_meter_ctrl = bm_ctrl_cmd,电池电量参数相关计算的一组函数接口
+
+    -> fg_custom_init_from_header 从头文件中获得电池参数 -> battery_update_routine,这是一个线程，是负责检测硬件电池参数变化，然后上报给power_supply
+
+    根据debug等级可以调整轮训时间 bat_get_debug_level() >= BMLOG_DEBUG_LEVEL -> (mtk_power_misc.c)shutdown_event_handler这个是关机检测执行的回调函数
+
+    -> battery_probe
+
+
+    fg这两个中断干什么？
+    {
+        fg_iavg_int_ht_handler      FG_IAVG_H_NO
+        fg_iavg_int_lt_handler      FG_IAVG_L_NO
+
+        应该是跟high，low相关的
+    }
+
+
+
+    控制fuel_gauge的log等级
+    FG_daemon_log_level
 
 }
 
