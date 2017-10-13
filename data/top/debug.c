7b28527701bfa63aebf91db770bde7d2d6150ddc
lilubao <lilubao@gionee.com>
2017-10-13 20:39:56 +0800

    add today work

--- data/top/debug.c
+++ data/top/debug.c
@@ -242,8 +242,209 @@
 			#define RT5081_PMU_REG_FLEDVMIDTRKCTRL2	(0x7C)
 	
 		}
+		
+		
+		
+		
+		邮件
+		{
+			Hi  樊工、宝玉;
+
+			1.目前在深圳这一台手机上验证，把VMID设定为4V，打闪100次Pass@VBAT=3.7V。
+
+			 
+
+			因为深圳测试端只有1台，无法多试几台，请北京这边帮忙试试看，先设定VMID=4V@VBAT=3.7V，打闪是否Pass？
+
+			a.如果Pass，那再设定VMID=4.1V or 4.2V@VBAT=3.7V，打闪是否Pass？
+
+			b.如果Fail，那再设定VMID=3.9V@VBAT=3.7V，打闪是否Pass？
+
+			 
+
+			2.线路上Battery到RT5081的VBAT有一颗Logic IC（SGM4075）做硬复位使用。
+
+			 
+
+			请问这颗Logic IC用途为何？为Battery硬复位还是为系统硬复位？
+
+			另外RT5081也有QonB 15s做系统Reset功能。
+			
+
+			3.北京测试反馈，电池电压会掉到3.32V，且拍照都不正常，这个电压没有低于先前提供的3.25V门限。
+
+		
+
+			4.一旦触发3.25V门限后，软件将会做什么动作。
+			
+			
+			
+			
+
+			韦工
+
+				 1、请问你们测试PASS的标准是什么？
+
+				 2、硬复位开关是在系统出现问题时可以强制断电的，我们需要所有点都断开，类似于拔掉电池，RT5081做不到。
+
+				 3、不管是否掉到门限电压，只要掉电压，我觉得就不是很正常，因为我们测试其他的项目是没有掉电压的，所以需要查明原因。如果是电流问题就修改电流，是配置问题就修改配置，现在原因还不清晰。
+
+			 
+
+			路宝
+
+				 回答一下第四条，谢谢！
+				 
+			 
+			 
+			 
+			 HI：韦工，宝玉
+			  3.25V对应的  LOW_BATTERY_LEVEL_1，触发了低电保护的中断，会累加g_low_battery_level同时会判断是电池是否真的耗完了
+			  LOW_BATTERY_LEVEL_1,会关掉了闪光灯和之前等级低的几个，CPU和GPU会限制performance,降频,降功率
+
+				  typedef enum LOW_BATTERY_PRIO_TAG {
+					LOW_BATTERY_PRIO_CPU_B = 0,
+					LOW_BATTERY_PRIO_CPU_L = 1,
+					LOW_BATTERY_PRIO_GPU = 2,
+					LOW_BATTERY_PRIO_MD = 3,
+					LOW_BATTERY_PRIO_MD5 = 4,
+					LOW_BATTERY_PRIO_FLASHLIGHT = 5,
+					LOW_BATTERY_PRIO_VIDEO = 6,
+					LOW_BATTERY_PRIO_WIFI = 7,
+					LOW_BATTERY_PRIO_BACKLIGHT = 8
+				} LOW_BATTERY_PRIO;
+
+
+			  低电保护level2的情况下，如果低电保护线程如果两次都检测到电池电压小于3.1V，就会调用关机
+			  /*2nd time receive battery voltage < 3.1V, wait FG to call power off */
+			 
+			  这个线程的检测时间轻负载的情况下是20s，正常是10s
+				 ktime = ktime_set(20, 0); /* light-loading mode */
+			  else
+				 ktime = ktime_set(10, 0); /* normal mode */
+				 
+				 
+				 
+				 1.请问你们测试PASS的标准是什么？
+
+				à拍照不会黑掉，而且主闪会在100ms左右；另外，VBAT-in不会掉到3.25V以下@VBAT=3.7V & VMID设定为4V (沈工与周工也有协助帮忙)
+
+				à波形如附件
+			
+			
+			
+			
+			
+			
+			
+			Hi  宝玉、路宝;
+
+
+			1.          关于贵司之前的项目打闪不会掉电池电压：
+
+			ð  请帮忙提供条件：电池电压/打闪电流
+
+			ð  请帮忙量测波形：VMID/ IBAT/VBAT/ILED (如果量不到的话，就量测VLED)
+
+			ð  相同条件下，务必两个项目一起量测才能比较
+
+			ð  是否可以提供之前的项目layout，并与该项目比较，确认FLED电流路径的trace是否雷同？
+
+			 
+
+			2.       关于打闪时，VMID的Overshoot：
+
+			ð  已提供EVB上不会有这个现象
+
+			ð  是否每一支都会这样，还是特例?
+
+			 
+
+			3.        关于低电压打闪会黑的问题：
+
+			ð  目前Layout上FLED电流路径的trace是否符合MTK建议 (之前有寄出的MTK report参考)
+
+			(尤其是BAT connect to Reset IC to RT5081 VBAT路径)
+
+			ð  另外请问调整VMID电压
+
+                 i.是否调整VMID到4V都pass？
+
+                 ii.是否可以多寄几支到深圳 ( 深圳只有一支，深圳金立其他同事也要用，FAE没办法做实验；目前深圳这支手机打闪电流不到1A，帮忙提供打闪电流达到1.5A的手机)
+
+
+
+
+			
+
+			韦工
+
+				 我们用同一块电池对比，在17G10A上有电压被拉低的现象，在其他项目上没有被拉低的现象，如果觉得这个实验还不够说明问题，请贵司派工作人员到我们这里现场指导一下如何测试。
+
+				 LAYOUT我们不能外发，贵司可以拍工作人员现场来看一下，我们自己对比觉得差异不大，请知悉！
+				 
+				 
+				 
+				 
+				 
+				 
+				 Hi  宝玉、樊工;
+
+					Q : 为何打闪会导致VBAT connector端 drop 200mV，这是否适合理现象?
+
+					A :
+
+					1.  这是合理现象，V=IR，由下列实验叙述验证
+
+					2.  电流部分：打闪电流Ch1(250mA) + Ch2(450mA)，IBAT实际量测1.2A
+
+					3.  内阻部分：电池package阻抗值(与樊工沟通后，查询了欣旺达ZCV阻抗值约160mΩ)
+
+					4.  电流*内阻=1.2A*160mΩ=192mV
+
+					 
+
+					Q : 有什么改善建议吗?
+
+					A :
+
+					1.  因为V=I*R, 只能由I & R做手脚
+
+					2.  FLED电流不能变动，因此建议修改FLED电压改低，以不影响打闪为前题(也就是不会压到headroom的意思)。
+
+					3.  因为目前电流软件设定不能超过1.1A，因此修改幅度有限
+
+					4.  因顾及贵司可能有固定使用电池，要不也可以考虑由阻抗(R)着手
+
+					 
+
+					Q : 除此之外，是否还有其他建议?
+
+					A :
+
+					1.  贵司目前把电池电流设定为最高1.1A，可能导致拍照亮度不足，请谨慎评估。
+
+					2.  与樊工沟通，如下Layout有下列几点建议
+
+					2-1.如下CHG_VMID到FL_VMID铜皮>1.5mm;
+					
+					
+		}
+		
 
 	}	
+	
+	
+	
+	
+	 平均电流测试自动化APK适配
+	
+	
+	
+	
+	
+	
+	
 
 
 }
@@ -276,18 +477,316 @@
 /*********************************************************************************************************************************/
 17G06A
 {
-	OTG
+
+
+	GNSPR#122265,连接充电器，长按电源键关机，关机完成后，长按电源键5s测机不开机，只显示在关机充电图标界面，（在充电图标界面长按电源键则可以开机），
+	用户体检不佳 暂未恢复 对比17G16-T0119版本有此现象，对比17G02-T2638版本无此现象，验证10台10台100%
 	{
-		GNSPR#120454_打开OTG开关未识别U盘
+	
+		
+						关机充电亮屏时间				 	关机充电灭屏到开机			关机充电灭屏状态下到开机
+		17G06A			8~9s						 	10~11s						4~5s
 		
 		
-	}
+		
+		
+		首先这个问题是：插充电器进入关机充电，亮屏状态下长按powerkey，进入正常的启动流程这个时间是4~5s正常
+		但是灭屏状态下，长按powerkey先是亮屏几秒，然后灭屏，过4~5s之后正常开机，总共10~11s，
+		
+		分析：
+			1.这种情况应该可以排除硬件复位，虽然时间是10+2s左右，硬件复位battery会断电
+			2.重启判断从第二次灭屏开始算的话，重启判断时间应该是相同的，
+			但是17G06A 关机充电灭屏状态下长按powerkey，先亮屏然后灭屏，再过几秒正常开机
+			
+			所以现在问题是灭屏状态下长按powerkey走了什么流程，怎么判断是短按还是长按
+			短按应该是亮屏
+			{
+				[   17.110191] *(1)[336:charger]charger: [17110] key[116] down
+
+				[   17.330015] *(1)[363:charger]msm_thermal:msm_thermal_update_freq Freq mitigation task is not initialized
+				[   17.344035] *1)PM: Some devices failed to suspend, or early wake event detected
+				[   17.352156] *(3)[363:charger]msm_thermal:msm_thermal_update_freq Freq mitigation task is not initialized
+
+				[   17.352197] *(0)[336:charger]charger: [17352] key[116] up (was down for 0.242sec)
+				
+				这几个在system 			healthd目录下
+				
+				
+				core/healthd/healthd.cpp
+				93:extern void healthd_mode_charger_init(struct healthd_config *config);
+				115:    .init = healthd_mode_charger_init,
+
+				core/healthd/healthd_mode_charger.cpp
+				1771:void healthd_mode_charger_init(struct healthd_config* config)
 
 
+				(healthd.cpp) process_key  这里可能有问题
+				next_pwr_check		
+ 				/* If key is pressed when the animation is not running, kick
+                 * the animation and quite suspend; If key is pressed when
+                 * the animation is running, turn off the animation and request
+                 * suspend.
+                 */
+	
+			}
+		
+		
+		
+		
+			
+		
+
+		KLOG_ERROR(LOG_TAG,"epoll_ctl failed; errno=%d\n", errno);
+		
+		
+		//Gionee <GN_BSP_CHG> <lilubao> <20171012> add for debug pon begin
+		pr_err("in [%s] by lilubao before\n",__FUNCTION__);
+		//Gionee <GN_BSP_CHG> <lilubao> <20171012> add for debug pon end
+		
+		
+		
+		boot_images/core/systemdrivers/pmic/target/msm8953_pm8953_pmi8950/system/src/pm_sbl_boot_oem.c
+		46:    err_flag |= pm_pon_reset_source_cfg(0, PM_PON_RESET_SOURCE_KPDPWR, 10256, 2000, PM_PON_RESET_CFG_WARM_RESET);
+		49:    err_flag |= pm_pon_reset_source_cfg(0, PM_PON_RESET_SOURCE_RESIN_AND_KPDPWR, 10256, 2000, PM_PON_RESET_CFG_HARD_RESET);
+
+		boot_images/core/systemdrivers/pmic/target/msm8937_pm8937_pmi8937/system/src/pm_sbl_boot_oem.c
+		70:    err_flag |= pm_pon_reset_source_cfg(0, PM_PON_RESET_SOURCE_KPDPWR, 10256, 2000, PM_PON_RESET_CFG_WARM_RESET);
+		72:    err_flag |= pm_pon_reset_source_cfg(0, PM_PON_RESET_SOURCE_KPDPWR, 6720, 2000, PM_PON_RESET_CFG_HARD_RESET);
+		77:    err_flag |= pm_pon_reset_source_cfg(0, PM_PON_RESET_SOURCE_RESIN_AND_KPDPWR, 10256, 2000, PM_PON_RESET_CFG_HARD_RESET);
+
+		boot_images/core/systemdrivers/pmic/drivers/pon/src/pm_pon.c
+		21:06/09/15   sv      Updated pm_pon_reset_source_cfg for multi PMIC support
+		531:pm_pon_reset_source_cfg(uint8 pmic_device_index, pm_pon_reset_source_type reset_source,
+
+		boot_images/core/systemdrivers/pmic/framework/test/pm_test_driver_pon.c
+		93:    err_flag = pm_pon_reset_source_cfg (pmic_index, PM_PON_RESET_SOURCE_RESIN_AND_KPDPWR, 3072, 1000, PM_PON_RESET_CFG_X_VDD_COIN_CELL_REMOVE_HARD_RESET);
+		226:            err_flag = pm_pon_reset_source_cfg (pmic_index, (pm_pon_reset_source_type)i, 3072, 1000, (pm_pon_reset_cfg_type)j);
+
+		boot_images/core/api/systemdrivers/pmic/pm_pon.h
+		276:pm_pon_reset_source_cfg(uint8 pmic_device_index, 
+
+
+		boot_images/core/storage/tools/deviceprogrammer_ddr/src/firehose/deviceprogrammer_initialize.c
+		120:    pm_pon_reset_source_cfg(0,PM_PON_RESET_SOURCE_KPDPWR,904,500,
+
+ 		
+		
+	
+	
+		(msm-poweroff.c)  do_msm_restart  -> msm_restart_prepare  ->   (qpnp-power-on.c)  qpnp_pon_system_pwr_off 
+		
+		
+		pm_pon.c,pm_sbl_boot_oem.c
 
 	
 
 
+
+
+
+
+
+
+
+
+
+
+		
+		80-nm620-1_b_pmic_pon-reset_software_drivers_overview.pdf
+		pm_pon.c,pm_pon.h，qpnp-power-on.c,msm-pm8937.dtsi,boot_dload_debug_target.c
+		
+		
+		int qpnp_pon_system_pwr_off() – Determines the type of power off to perform:
+shutdown, reset, etc.
+
+
+		This code lets us check whether the PMIC went through a warm reset
+		/drivers/power/qpnp-bms.c ,read_soc_params_raw(...){
+		
+		
+		- qcom,pon-type			The type of PON/RESET source. The driver currently supports KPDPWR(0), RESIN(1) and
+		CBLPWR(2) pon/reset sources.
+		
+		
+		
+		PON_XXXX_RESET_S1_TIMER
+		PON_XXXX_RESET_S2_TIMER
+		PON_XXXX_RESET_S2_CTL
+		PON_XXXX_RESET_S2_CTL2
+		
+		
+		重启的阶段
+		Every reset happens in different stages and each stage timer is
+		configurable.
+
+		Stage 1 – For each of the triggers, an interrupt is sent to the MSM to warn it about the
+		shutdown. We can configure the amount of time (S1_Timer) the debounced trigger must
+		be held before a bark is sent to the MSM using this register:
+		PON_XXX_RESET_S1_TIMER.
+		Stage 2 – Once the interrupt is sent to the MSM, the PMIC waits for a reply for S2_TIMER
+		period before it performs the desired shutdown. We can configure the amount of time the
+		debounced trigger using this register: PON_XXX_RESET_S2_TIMER.
+		Stage 3 – This stage is independent of S1 and S2. If trigger is held for an extended period
+		of time (default 64 sec), a forced xVdd shutdown occurs. Which means it is an abrupt
+		power down and it does not follow a graceful shutdown procedure.
+
+		PAGE 20
+		This guarantees that there is a way out for unexpected hangups
+		Reset all PMIC registers
+		
+		
+		Vdd这几个是干什么的？
+		
+		
+		
+		
+		
+		
+			关机充电的流程
+			{
+				Android Bootloader - UART_DM Initialized!!!
+				[0] welcome to lk
+
+				[10] platform_init()
+				[10] target_init()
+	
+	
+				[40] pm8x41_get_is_cold_boot: cold boot
+				[40] Qseecom Init Done in Appsbl
+	
+	
+				[90] Waiting for the RPM to populate smd channel table
+	
+	
+				高通关机重启
+				/* In case of fastboot reboot,adb reboot or if we see the power key
+				* pressed we do not want go into charger mode.
+				* fastboot reboot is warm boot with PON hard reset bit not set
+				* adb reboot is a cold boot with PON hard reset bit set
+				*/
+	
+				80-nm620-1_b_pmic_pon-reset_software_drivers_overview.pdf
+	
+				重启的类型：OTST3, KPD_PWR_N, PS_HOLD
+	
+	
+				dVdd_rb，xVdd_rb
+	
+				pon_resin_n_reset_s1_timer
+	
+	
+				Kernel command line: sched_enable_hmp=1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 
+				androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 buildvariant=eng androidboot.emmc=true androidboot.verifiedbootstate=green androidboot.veritymode=enforcing 
+
+				androidboot.keymaster=1 androidboot.serialno=665cd078
+
+				androidboot.mode=charger androidboot.authorized_kernel=true androidboot.baseband=msm printk.disable_uart=9 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_ft8006m_720p_video:1:none:cfg:single_dsi
+
+				这些参数在 （aboot.c）unsigned char *update_cmdline(const char * cmdline) , 获取cmdline
+				（init.c）target_pause_for_battery_charge获取关机状态插入充电器的动作还是fastboot，adb等pon	
+				init.c文件还有获取组合按键动作
+
+				Every reset happens in different stages and each stage timer is
+				configurable.
+
+				Stage 1 – For each of the triggers, an interrupt is sent to the MSM to warn it about the
+				shutdown. We can configure the amount of time (S1_Timer) the debounced trigger must
+				be held before a bark is sent to the MSM using this register:
+				PON_XXX_RESET_S1_TIMER.
+				Stage 2 – Once the interrupt is sent to the MSM, the PMIC waits for a reply for S2_TIMER
+				period before it performs the desired shutdown. We can configure the amount of time the
+				debounced trigger using this register: PON_XXX_RESET_S2_TIMER.
+				Stage 3 – This stage is independent of S1 and S2. If trigger is held for an extended period
+				of time (default 64 sec), a forced xVdd shutdown occurs. Which means it is an abrupt
+				power down and it does not follow a graceful shutdown procedure.
+
+				PAGE 20
+				This guarantees that there is a way out for unexpected hangups
+				Reset all PMIC registers	
+			}
+		
+	
+	}
+
+
+
+
+
+
+
+
+	USB类
+	{
+		
+		GNSPR#119978,连接充电器，待机界面》连接电脑传输文件后断开连接，主界面下滑通知栏仍显示有USB传输文件，点击USB传输文件闪退至桌面，
+		操作多次未恢复，重新连接电脑再断开未恢复，手动重启恢复》
+		
+
+	}
+
+
+
+
+
+
+
+
+
+	
+	可能跟硬件相关的
+	{
+		GNSPR#119926,T2-2-228,BJ17G06A-T0050-170930AA ,吴能田
+		这个可能跟NTC有关，NTC异常，因为开机后温度升到很高，
+				
+		请帮忙拆机测量一下NTC是否正常，电池连接器是否扣好
+		然后用示波器测量一下NTC测试点的波形，如果正常应该是1.5s一次脉冲波
+		没扣好会有1~2V的高电平，短路或者其他的，不会有1.5s的脉冲
+		
+		
+		GNSPR#121951,T2-2-37,BJ17G06A-T0050-170930AA,陈艳，
+		GNSPR#119947，T2-2-88,BJ17G06A-T0050-170930AA,吴 能田 
+		这个可能是电池连接器没扣好，我本地试了，不扣电池，插充电器，电量显示50%，而且是关机充电不能开机
+		
+		
+		
+		GNSPR#119926 关机状态》(电量剩余：40%）手动按电源键开机后，手机自动关机，操作多次仍如此，暂未恢复（串口log）》验证2台，1台100% 
+		{
+
+			怀疑NTC有问题，电池温度在开机之后一段时间就变的很高，然后温度过高导致关机
+
+			[ 94.207274] *(1)[96:kworker/1:1]FG: log_bat_status: soc=3885 vbat=3754250 ibat=310361 ocv=3801705 esr=109008 temp=402 vchg=246600
+			[ 104.246362] *(1)[96:kworker/1:1]FG: log_bat_status: soc=3882 vbat=3749978 ibat=347288 ocv=3801705 esr=111145 temp=402 vchg=246600
+			[ 114.286314] *(1)[96:kworker/1:1]FG: log_bat_status: soc=3880 vbat=3722817 ibat=597530 ocv=3801705 esr=110168 temp=405 vchg=244640
+			[ 124.329015] *(1)[96:kworker/1:1]FG: log_bat_status: soc=3877 vbat=3697183 ibat=827021 ocv=3801705 esr=113525 temp=402 vchg=240740
+			[ 2.299768] *(1)[1:swapper/0]BOOTPROF: 2299.762350:probe: probe=spmi_drv_probe drv=qcom,qpnp-temp-alarm(ffffffc00165ab60) 15.608489ms
+			[ 3.789490] *(0)[54:kworker/0:1]FG: log_bat_status: soc=3874 vbat=3740822 ibat=432889 ocv=3801705 esr=110412 temp=-22 vchg=242720
+			[ 3.809878] *(0)[271:kworker/0:2]FG: log_bat_status: soc=3874 vbat=3740822 ibat=432889 ocv=3801705 esr=110412 temp=400 vchg=242720
+			[ 13.845252] *(0)[271:kworker/0:2]FG: log_bat_status: soc=3869 vbat=3685128 ibat=926813 ocv=3801705 esr=110412 temp=405 vchg=240780
+			[ 23.939435] *(0)[271:kworker/0:2]FG: log_bat_status: soc=3864 vbat=3704202 ibat=737452 ocv=3801705 esr=107971 temp=405 vchg=242720
+			[ 33.967626] *(0)[271:kworker/0:2]FG: log_bat_status: soc=3860 vbat=3742653 ibat=389402 ocv=3801705 esr=109069 temp=405 vchg=244680
+			[ 35.763924] *(2)[1:init]BOOTPROF: [6/6].PON battery temperature : 400
+			[ 44.007872] *(0)[271:kworker/0:2]FG: log_bat_status: soc=3855 vbat=3707101 ibat=695186 ocv=3801705 esr=109069 temp=405 vchg=242720
+			[ 54.051098] *(0)[271:kworker/0:2]FG: log_bat_status: soc=3850 vbat=3706795 ibat=687251 ocv=3801705 esr=109802 temp=882 vchg=244680
+			[ 2.230797] *(0)[1:swapper/0]BOOTPROF: 2230.791360:probe: probe=spmi_drv_probe drv=qcom,qpnp-temp-alarm(ffffffc00165ab60) 15.605937ms
+			[ 3.718023] *(2)[97:kworker/2:1]FG: log_bat_status: soc=3845 vbat=3738839 ibat=425870 ocv=3801705 esr=110229 temp=-22 vchg=244640
+			[ 3.737546] *(2)[292:kworker/2:2]FG: log_bat_status: soc=3845 vbat=3738839 ibat=425870 ocv=3801705 esr=110229 temp=407 vchg=244640
+			[ 13.764648] *(2)[292:kworker/2:2]FG: log_bat_status: soc=3840 vbat=3682687 ibat=921167 ocv=3801705 esr=110961 temp=407 vchg=244640
+			[ 23.804627] *(2)[292:kworker/2:2]FG: log_bat_status: soc=3836 vbat=3700692 ibat=752711 ocv=3799721 esr=111877 temp=925 vchg=244640
+			[ 33.846244] *(2)[292:kworker/2:2]FG: log_bat_status: soc=3834 vbat=3755166 ibat=248411 ocv=3799721 esr=112243 temp=905 vchg=248560
+
+
+			请帮忙拆机测量一下NTC是否正常，电池连接器是否扣好
+			然后用示波器测量一下NTC测试点的波形，如果正常应该是1.5s一次脉冲波
+			没扣好会有1~2V的高电平，短路或者其他的，不会有1.5s的脉冲
+	
+		}
+		
+		
+	}
+	
+	
+	
+	
 
 
 
@@ -328,74 +827,6 @@
 	}	
 	
 	
-	
-	
-	
-		
-	关机充电的流程
-	{
-		Android Bootloader - UART_DM Initialized!!!
-		[0] welcome to lk
-
-		[10] platform_init()
-		[10] target_init()
-	
-	
-		[40] pm8x41_get_is_cold_boot: cold boot
-		[40] Qseecom Init Done in Appsbl
-	
-	
-		[90] Waiting for the RPM to populate smd channel table
-	
-	
-		高通关机重启
-		/* In case of fastboot reboot,adb reboot or if we see the power key
-		* pressed we do not want go into charger mode.
-		* fastboot reboot is warm boot with PON hard reset bit not set
-		* adb reboot is a cold boot with PON hard reset bit set
-		*/
-	
-		80-nm620-1_b_pmic_pon-reset_software_drivers_overview.pdf
-	
-		重启的类型：OTST3, KPD_PWR_N, PS_HOLD
-	
-	
-		dVdd_rb，xVdd_rb
-	
-		pon_resin_n_reset_s1_timer
-	
-	
-		Kernel command line: sched_enable_hmp=1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 
-		androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 buildvariant=eng androidboot.emmc=true androidboot.verifiedbootstate=green androidboot.veritymode=enforcing 
-
-		androidboot.keymaster=1 androidboot.serialno=665cd078
-
-		androidboot.mode=charger androidboot.authorized_kernel=true androidboot.baseband=msm printk.disable_uart=9 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_ft8006m_720p_video:1:none:cfg:single_dsi
-
-		这些参数在 （aboot.c）unsigned char *update_cmdline(const char * cmdline) , 获取cmdline
-		（init.c）target_pause_for_battery_charge获取关机状态插入充电器的动作还是fastboot，adb等pon	
-		init.c文件还有获取组合按键动作
-
-		Every reset happens in different stages and each stage timer is
-		configurable.
-
-		Stage 1 – For each of the triggers, an interrupt is sent to the MSM to warn it about the
-		shutdown. We can configure the amount of time (S1_Timer) the debounced trigger must
-		be held before a bark is sent to the MSM using this register:
-		PON_XXX_RESET_S1_TIMER.
-		Stage 2 – Once the interrupt is sent to the MSM, the PMIC waits for a reply for S2_TIMER
-		period before it performs the desired shutdown. We can configure the amount of time the
-		debounced trigger using this register: PON_XXX_RESET_S2_TIMER.
-		Stage 3 – This stage is independent of S1 and S2. If trigger is held for an extended period
-		of time (default 64 sec), a forced xVdd shutdown occurs. Which means it is an abrupt
-		power down and it does not follow a graceful shutdown procedure.
-
-		PAGE 20
-		This guarantees that there is a way out for unexpected hangups
-		Reset all PMIC registers	
-	}
-
-
 }
 
 
